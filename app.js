6cde87578abbd35d877d5a3e72bf4487d96f6e90
Bertwin Romero <bertwinr2@gmail.com>
2020-04-17 09:13:01 +0800

    feat(): created user routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const morgan = require("morgan");
+
+const usersRoutes = require('./src/api/components/users');
 
 dotenv.config();
 
@@ -10,7 +13,15 @@ mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log('connected to db!'));
 
+app.use(morgan("dev"));
+
+
 // MIDDLEWARS
 app.use(express.json());
+
+// ROUTES MIDDLEWARE
+app.use('/api/users', usersRoutes);
+
+console.log(process.env.PORT)
 
 module.exports = app;
