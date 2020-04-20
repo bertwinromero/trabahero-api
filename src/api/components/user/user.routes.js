67bd4e99eb7c4464aa8797cbf946c2f36f5e1b4b
Bertwin Romero <bertwinr2@gmail.com>
2020-04-20 19:22:18 +0800

    feat(): user private routes

--- src/api/components/user/user.routes.js
+++ src/api/components/user/user.routes.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 const userController = require('./user.controller');
 const validate = require('../../middlewares/validation');
+const verifyToken =  require('../../middlewares/verify-token');
 
-router.get('/', userController.getUsers);
+router.get('/', verifyToken, userController.getUsers);
 
-router.get('/:id', userController.getUser);
+router.get('/:id', verifyToken, userController.getUser);
 
 router.post('/signup', validate.createUserValidation, userController.createUser);
 
 router.post('/signin', validate.signinUserValidation, userController.signUser);
 
-router.patch('/:id', validate.updateUserValidation, userController.updateUser);
+router.patch('/:id', verifyToken, validate.updateUserValidation, userController.updateUser);
 
-router.delete('/:id', userController.deleteUser);
+router.delete('/:id', verifyToken, userController.deleteUser);
 
 module.exports = router;
