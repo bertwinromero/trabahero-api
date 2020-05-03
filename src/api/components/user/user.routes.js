a41705d95c74d63aa4e24f4e9c90d20064af2131
Bertwin Romero <bertwinr2@gmail.com>
2020-05-03 12:59:41 +0800

    feat(): upload config file

--- src/api/components/user/user.routes.js
+++ src/api/components/user/user.routes.js
@@ -2,33 +2,9 @@ const router = require('express').Router();
 const userController = require('./user.controller');
 const validate = require('../../middlewares/validations/validate-user');
 const verifyToken =  require('../../middlewares/verify-token');
-const multer = require('multer');
+const upload = require('../../../config/upload.config').upload;
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './src/uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-  }
-});
 
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
 router.get('/', verifyToken, userController.getUsers);
 
 router.get('/:id', verifyToken, userController.getUser);
